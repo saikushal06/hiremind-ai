ee90eac796421d6879e5931a9942dc4a4eefc290
Sai Kushal Ranga <saikushal467@gmail.com>
2026-05-18 18:52:42 +0530

    Secure Gemini API key with env

--- src/gemini.js
+++ src/gemini.js
@@ -1,6 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyAAztRX_QUf2GymNasPHPQCu4-xW2nQKaU");
+const genAI = new GoogleGenerativeAI(
+  import.meta.env.VITE_GEMINI_API_KEY
+);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
